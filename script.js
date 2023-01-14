747d3c82c882a9efcca63092941a8fe344615deb
Abhishek <idiot63666@gmail.com>
2023-01-14 22:04:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const cards = document.querySelectorAll(".card");
 
+let cardOne, cardTwo
+
+
 function flipCard(e) {
-    console.log(e.target);
+    let clickedCard = e.target; //getting user clicked card
+    clickedCard.classList.add("flip");
+    if (clickedCard !== cardOne) {
+        if (!cardOne) {
+            //return the cardOne value to clickedCard
+            return cardOne = clickedCard;
+        }
+        cardTwo = clickedCard;
+        console.log(cardOne, cardTwo);
+    }
 }
 
 cards.forEach(card => { // adding click event to all cards
